bf2ceccdb900c3291fce6a0cfc4fa09ce73f06a1
Mikolaj <mikolajpaz@gmail.com>
2025-04-16 09:27:42 +0200

    Add initial weights

--- models/srcnn.py
+++ models/srcnn.py
@@ -8,9 +8,15 @@ import torch.nn.functional as F
 class SRCNN(nn.Module):
     def __init__(self, upscale_factor=2):
         super(SRCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 128, kernel_size=9, padding=2)
-        self.conv2 = nn.Conv2d(128, 64, kernel_size=1, padding=2)
-        self.conv3 = nn.Conv2d(64, 3, kernel_size=5, padding=2)
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+
+        for layer in [self.conv1, self.conv2, self.conv3]:
+            nn.init.normal_(self.conv3.weight, mean=.0, std=.001)
+            if layer.bias is not None:
+                nn.init.constant_(layer.bias, 0.0)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
