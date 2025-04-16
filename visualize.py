fa95e9365d4306acb4d379e4f93ab14559638014
Mikolaj <mikolajpaz@gmail.com>
2025-04-16 09:29:47 +0200

    Add visualization script for SRCNN model outputs

---
+++ visualize.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from torchvision.transforms import ToPILImage
+from PIL import Image
+
+from models.srcnn import SRCNN
+from models.srgan import SRGANgenerator, SRGANdiscriminator
+from models.swinir import SwinIR
+
+def model_from_file(model: nn.Module, path: str):
+    try:
+        model.load_state_dict(torch.load(path, weights_only=True))
+        print(f'Loaded model from {path}')
+    except FileNotFoundError:
+        print(f'Couldn\'t load the model from {path}')
+
+def image_output_from_model(model: nn.Module, device: torch.device, img: Image):
+    model = model.to(device)
+    tensor: torch.Tensor = transforms.ToTensor()(img).to(device)
+
+    if type(model) is SRCNN:
+        tensor = nn.Upsample(scale_factor=2, mode='bicubic')(tensor.unsqueeze(0))
+
+    with torch.no_grad():
+        print(f'Calculating {model._get_name()} output...')
+        output: torch.Tensor = model(tensor)
+
+    return ToPILImage()(output.squeeze(0).cpu())
+
+def visualize_all_models(srcnn_src: str, device: torch.device, hr: Image, lr: Image):
+    srcnn = SRCNN().to(device)
+    model_from_file(srcnn, srcnn_src)
+
+    out_srcnn = image_output_from_model(srcnn, device, lr)
+
+    plt.subplot(1, 2, 1)
+    plt.title('High Resolution Image')
+    plt.imshow(hr)
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.title('SRCNN Output Image')
+    plt.imshow(out_srcnn)
+    plt.axis('off')
+
+    plt.show()
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--srcnn', type=str, required=True)
+    parser.add_argument('--lr', type=str, required=True)
+    parser.add_argument('--hr', type=str, required=True)
+    args = parser.parse_args()
+
+    device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    hr_images = sorted(os.listdir(args.hr))
+    lr_images = sorted(os.listdir(args.lr))
+
+    index = np.random.randint(0, len(hr_images))
+
+    hr_path = os.path.join(args.hr, hr_images[index])
+    lr_path = os.path.join(args.lr, lr_images[index])
+
+    hr_img = Image.open(hr_path).convert("RGB")
+    lr_img = Image.open(lr_path).convert("RGB")
+
+    visualize_all_models(args.srcnn, device, hr_img, lr_img)
