2d01a6c78e1493d43ee48f1201d1427b248392b5
Mikolaj <mikolajpaz@gmail.com>
2025-04-17 22:42:01 +0200

    Refactor visualize_all_models to include SwinIR

--- visualize.py
+++ visualize.py
@@ -20,10 +20,10 @@ def model_from_file(model: nn.Module, path: str):
 
 def image_output_from_model(model: nn.Module, device: torch.device, img: Image):
     model = model.to(device)
-    tensor: torch.Tensor = transforms.ToTensor()(img).to(device)
+    tensor: torch.Tensor = transforms.ToTensor()(img).to(device).unsqueeze(0)
 
     if type(model) is SRCNN:
-        tensor = nn.Upsample(scale_factor=2, mode='bicubic')(tensor.unsqueeze(0))
+        tensor = nn.Upsample(scale_factor=2, mode='bicubic')(tensor)
 
     with torch.no_grad():
         print(f'Calculating {model._get_name()} output...')
@@ -31,20 +31,29 @@ def image_output_from_model(model: nn.Module, device: torch.device, img: Image):
 
     return ToPILImage()(output.squeeze(0).cpu())
 
-def visualize_all_models(srcnn_src: str, device: torch.device, hr: Image, lr: Image):
+def visualize_all_models(srcnn_src: str, swinir_src: str, device: torch.device, hr: Image, lr: Image):
     srcnn = SRCNN().to(device)
     model_from_file(srcnn, srcnn_src)
 
-    out_srcnn = image_output_from_model(srcnn, device, lr)
+    swinir = SwinIR().to(device)
+    model_from_file(swinir, swinir_src)
 
-    plt.subplot(1, 2, 1)
+    out_srcnn = image_output_from_model(srcnn, device, lr)
+    out_swinir = image_output_from_model(swinir, device, lr)
+
+    plt.subplot(1, 3, 1)
     plt.title('High Resolution Image')
     plt.imshow(hr)
     plt.axis('off')
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.title('SRCNN Output Image')
     plt.imshow(out_srcnn)
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.title('SwinIR Output Image')
+    plt.imshow(out_swinir)
     plt.axis('off')
 
     plt.show()
@@ -54,6 +63,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--srcnn', type=str, required=True)
+    parser.add_argument('--swinir', type=str, required=True)
     parser.add_argument('--lr', type=str, required=True)
     parser.add_argument('--hr', type=str, required=True)
     args = parser.parse_args()
@@ -71,4 +81,4 @@ if __name__ == '__main__':
     hr_img = Image.open(hr_path).convert("RGB")
     lr_img = Image.open(lr_path).convert("RGB")
 
-    visualize_all_models(args.srcnn, device, hr_img, lr_img)
+    visualize_all_models(args.srcnn, args.swinir, device, hr_img, lr_img)
