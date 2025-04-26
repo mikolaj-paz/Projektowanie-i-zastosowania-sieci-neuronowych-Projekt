dbb8746b110701e06a7cc1caddbb5be8673591d1
Mikolaj <mikolajpaz@gmail.com>
2025-04-26 21:43:20 +0200

    Enhance visualization function to include SRGAN output

--- visualize.py
+++ visualize.py
@@ -31,27 +31,36 @@ def image_output_from_model(model: nn.Module, device: torch.device, img: Image):
 
     return ToPILImage()(output.squeeze(0).cpu())
 
-def visualize_all_models(srcnn_src: str, swinir_src: str, device: torch.device, hr: Image, lr: Image):
+def visualize_all_models(srcnn_src: str, srgan_src: str, swinir_src: str, device: torch.device, hr: Image, lr: Image):
     srcnn = SRCNN().to(device)
     model_from_file(srcnn, srcnn_src)
+
+    srgan = SRGANgenerator().to(device)
+    model_from_file(srgan, srgan_src)
 
     swinir = SwinIR().to(device)
     model_from_file(swinir, swinir_src)
 
     out_srcnn = image_output_from_model(srcnn, device, lr)
+    out_srgan = image_output_from_model(srgan, device, lr)
     out_swinir = image_output_from_model(swinir, device, lr)
 
-    plt.subplot(1, 3, 1)
+    plt.subplot(2, 2, 1)
     plt.title('High Resolution Image')
     plt.imshow(hr)
     plt.axis('off')
 
-    plt.subplot(1, 3, 2)
+    plt.subplot(2, 2, 2)
     plt.title('SRCNN Output Image')
     plt.imshow(out_srcnn)
     plt.axis('off')
 
-    plt.subplot(1, 3, 3)
+    plt.subplot(2, 2, 3)
+    plt.title('SRGAN Output Image')
+    plt.imshow(out_srgan)
+    plt.axis('off')
+
+    plt.subplot(2, 2, 4)
     plt.title('SwinIR Output Image')
     plt.imshow(out_swinir)
     plt.axis('off')
@@ -63,6 +72,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--srcnn', type=str, required=True)
+    parser.add_argument('--srgan', type=str, required=True)
     parser.add_argument('--swinir', type=str, required=True)
     parser.add_argument('--lr', type=str, required=True)
     parser.add_argument('--hr', type=str, required=True)
@@ -81,4 +91,4 @@ if __name__ == '__main__':
     hr_img = Image.open(hr_path).convert("RGB")
     lr_img = Image.open(lr_path).convert("RGB")
 
-    visualize_all_models(args.srcnn, args.swinir, device, hr_img, lr_img)
+    visualize_all_models(args.srcnn, args.srgan, args.swinir, device, hr_img, lr_img)
